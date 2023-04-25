1a68ae721d5916a57e0fe6a2eaa539881a553cf8
Simon Blöchinger <simon.bloechinger@tum.de>
2023-04-25 12:30:16 +0200

    fixed issues with the paths for the szenarios to reflect the folder structure in the gitlab

--- exercise1/Task 1/main.py
+++ exercise1/Task 1/main.py
@@ -16,14 +16,14 @@ def main():
     pygame.display.set_caption("Crowd Simulation")
 
     # Load the initial scenario
-    grid = load_scenario("Exercise1/Task 1/Scenarios/scenario.json", GRID_SIZE, CELL_SIZE)
+    grid = load_scenario("./Scenarios/scenario.json", GRID_SIZE, CELL_SIZE)
 
     # Initialize buttons
     next_step_button = Button(10, HEIGHT - 60, 100, 40, "Next Step", (200, 200, 200))
     choose_scenario_button = ChooseScenarioButton(120, HEIGHT - 60, 200, 40, "Choose Scenario", (200, 200, 200))
 
     # Initialize the file dialog
-    file_dialog = FileDialog("Exercise1/Task 1/Scenarios", ".json", screen)
+    file_dialog = FileDialog("./Scenarios", ".json", screen)
 
     step_count = 0
 
