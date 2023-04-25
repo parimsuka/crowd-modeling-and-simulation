286cdc7f1a2fa9bee8de8df292fd2648dc5f31bd
Simon Blöchinger <simon.bloechinger@tum.de>
2023-04-25 12:57:05 +0200

    removed capitalization of scenarios folder

--- exercise1/src/main.py
+++ exercise1/src/main.py
@@ -18,14 +18,14 @@ def main():
     pygame.display.set_caption("Crowd Simulation")
 
     # Load the initial scenario
-    grid = load_scenario("Scenarios/scenario-default.json", GRID_SIZE, CELL_SIZE)
+    grid = load_scenario("scenarios/scenario-default.json", GRID_SIZE, CELL_SIZE)
 
     # Initialize buttons
     next_step_button = Button(10, HEIGHT - 60, 100, 40, "Next Step", (200, 200, 200))
     choose_scenario_button = ChooseScenarioButton(120, HEIGHT - 60, 200, 40, "Choose Scenario", (200, 200, 200))
 
     # Initialize the file dialog
-    file_dialog = FileDialog("./Scenarios", ".json", screen)
+    file_dialog = FileDialog("./scenarios", ".json", screen)
 
     step_count = 0
 
