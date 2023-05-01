17ba4016a584ed4a3d78a1ab11ad54450bed8bb9
Simon Blöchinger <simon.bloechinger@tum.de>
2023-05-01 03:47:07 +0200

    Fixed bias of pedestrians towards certain directions when two cells had the same score by randomly shuffeling the arrays

--- exercise1/src/pedestrian.py
+++ exercise1/src/pedestrian.py
@@ -262,10 +262,13 @@ class Pedestrian:
         # Variable containing all neighboring cells, starting with the current cell
         neighbor_cells = [[int_x, int_y, "current"]]
 
-        for i, j in [
-            [1, 0], [0, 1], [-1, 0], [0, -1],
-            [-1, -1], [-1, 1], [1, -1], [1, 1]
-        ]:
+        horizontal_vertical_cells = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        diagonal_cells = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+
+        np.random.shuffle(horizontal_vertical_cells)
+        np.random.shuffle(diagonal_cells)
+
+        for i, j in horizontal_vertical_cells + diagonal_cells:
             cell_x = int_x+i
             cell_y = int_y+j
             if 0 <= cell_x < len(grid) and 0 <= cell_y < len(grid[0]):
